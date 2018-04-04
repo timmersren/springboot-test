99a82a0fa0fa20a012f281663e6a8112b4e249ac
任伟 <459001452@qq.com>
2018-04-04 09:51:49 +0800

    配置文件加载的优先级 配置互补

---
+++ springboot-02-config2/src/main/java/com/rain/Springboot02Config2Application.java
@@ -0,0 +1,12 @@
+package com.rain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02Config2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot02Config2Application.class, args);
+	}
+}
