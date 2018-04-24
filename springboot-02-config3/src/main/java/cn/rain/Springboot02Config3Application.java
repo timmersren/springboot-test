72214c4fd32e7509bf41cf975e1d8eb23a0c345d
任伟 <459001452@qq.com>
2018-04-24 23:55:46 +0800

    自动配置原理

---
+++ springboot-02-config3/src/main/java/cn/rain/Springboot02Config3Application.java
@@ -0,0 +1,12 @@
+package cn.rain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02Config3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot02Config3Application.class, args);
+    }
+}
---
+++ springboot-02-config3/src/test/java/cn/rain/springboot02config3/Springboot02Config3ApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.rain.springboot02config3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot02Config3ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
