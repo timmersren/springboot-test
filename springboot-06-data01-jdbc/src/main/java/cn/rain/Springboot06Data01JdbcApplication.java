4355f7770461880c4fde0181e83a888f4e3af119
任伟 <459001452@qq.com>
2018-05-06 01:03:16 +0800

    create springboot-06-data01-jdbc

---
+++ springboot-06-data01-jdbc/src/main/java/cn/rain/Springboot06Data01JdbcApplication.java
@@ -0,0 +1,12 @@
+package cn.rain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot06Data01JdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot06Data01JdbcApplication.class, args);
+    }
+}
