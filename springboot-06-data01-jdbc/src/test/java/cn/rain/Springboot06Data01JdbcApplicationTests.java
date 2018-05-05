c888557ea06595b2a659ac8560c03a37aa9ce6d0
任伟 <459001452@qq.com>
2018-05-06 02:04:45 +0800

    test jdbc

--- springboot-06-data01-jdbc/src/test/java/cn/rain/Springboot06Data01JdbcApplicationTests.java
+++ springboot-06-data01-jdbc/src/test/java/cn/rain/Springboot06Data01JdbcApplicationTests.java
@@ -2,15 +2,37 @@ package cn.rain;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Springboot06Data01JdbcApplicationTests {
 
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Test
-    public void contextLoads() {
+    public void testJdbcConnection() throws SQLException {
+        System.out.println(dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
     }
 
+    @Test
+    public void testGetData(){
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("SELECT * FROM department");
+        System.out.println(list.toString());
+    }
 }
